53ab0a97f3a43748c5de22b08858e3ef25e4980b
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-03-03 18:03:31 -0300

    Added multer to get img files from front end posts

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+const multer = require('multer');
+
 
 dotenv.config();
 mongoose.set("strictQuery", false);
@@ -17,6 +19,23 @@ mongoose.connect(process.env.MONGO_URL, () =>{
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename:(req, file, cb ) => {
+        cb(null, req.body.name)
+    }
+})
+
+const upload = multer({storage: storage});
+app.post('api/upload', upload.single("file"), (req, res) =>{
+    try{
+        return res.status(200).json("file uploaded successfully")
+    }catch(err){
+        console.log(err)
+    }
+})
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
