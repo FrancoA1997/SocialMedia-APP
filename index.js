d9362ead66fd838da48fff869f4d7f620021207d
Franco Alvarez <98923793+FrancoA1997@users.noreply.github.com>
2023-03-06 18:55:15 -0300

    Get followers end-point added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
 const multer = require('multer');
+const path = require("path");
+
 
 
 dotenv.config();
@@ -15,21 +17,22 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, () =>{
     console.log("Connected to MongoDB")
 });
-
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "public/images");
     },
     filename:(req, file, cb ) => {
-        cb(null, req.body.name)
-    }
-})
+        cb(null, req.body.name1 )
+    },
+});
 
 const upload = multer({storage: storage});
-app.post('api/upload', upload.single("file"), (req, res) =>{
+app.post('/api/upload', upload.single("file"), (req, res) =>{
     try{
         return res.status(200).json("file uploaded successfully")
     }catch(err){
